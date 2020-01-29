179f3775035cf0d1f5cada63dc0071ff440bbb28
klcantrell <cantrellkalalau@gmail.com>
2020-01-28 22:24:10 -0500

    Animate input in and out

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,19 +1,54 @@
 import React, { FunctionComponent } from 'react';
-import {
-  StyleSheet,
-  KeyboardAvoidingView,
-  TextInput as ReactNativeTextInput,
-} from 'react-native';
-import {
-  View,
-  Button,
-  Icon,
-  Text,
-  Form,
-  Item,
-  Label,
-  Input,
-} from 'native-base';
+import { StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { View, Button, Icon, Text, Form, Item, Input } from 'native-base';
+import Animated, { Easing } from 'react-native-reanimated';
+
+const {
+  Clock,
+  Value,
+  set,
+  cond,
+  startClock,
+  clockRunning,
+  timing,
+  stopClock,
+  block,
+} = Animated;
+
+const INPUT_HIDDEN_OFFSET = -500;
+
+const runTiming = (clock, value, dest, easing) => {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+
+  const config = {
+    duration: 300,
+    toValue: new Value(0),
+    easing,
+  };
+
+  return block([
+    cond(
+      clockRunning(clock),
+      [set(config.toValue, dest)],
+      [
+        set(state.finished, 0),
+        set(state.time, 0),
+        set(state.position, value),
+        set(state.frameTime, 0),
+        set(config.toValue, dest),
+        startClock(clock),
+      ]
+    ),
+    timing(clock, state, config),
+    cond(state.finished, stopClock(clock)),
+    state.position,
+  ]);
+};
 
 interface Props {
   handleAdd: () => void;
@@ -22,13 +57,25 @@ interface Props {
 const Poster: FunctionComponent<Props> = ({ handleAdd }) => {
   const [inputVisible, setInputVisible] = React.useState(false);
 
-  const textInput = React.useRef<ReactNativeTextInput>(null);
+  const textInput = React.useRef(null);
 
   React.useEffect(() => {
-    if (inputVisible && textInput.current) {
-      textInput.current.focus();
+    if (inputVisible && textInput.current && textInput.current._root) {
+      textInput.current._root.focus();
     }
   }, [inputVisible]);
+
+  let transX;
+  if (inputVisible) {
+    transX = runTiming(new Clock(), -500, 0, Easing.inOut(Easing.ease));
+  } else {
+    transX = runTiming(
+      new Clock(),
+      0,
+      INPUT_HIDDEN_OFFSET,
+      Easing.inOut(Easing.exp)
+    );
+  }
 
   const toggleInput = () => setInputVisible(!inputVisible);
 
@@ -37,26 +84,26 @@ const Poster: FunctionComponent<Props> = ({ handleAdd }) => {
       <KeyboardAvoidingView
         style={styles.keyboardView}
         behavior="position"
-        keyboardVerticalOffset={-70}
+        keyboardVerticalOffset={-68}
         pointerEvents="box-none"
       >
-        {inputVisible && (
-          <Form style={styles.form}>
-            <Item style={styles.inputGroup} floatingLabel>
-              <Label style={styles.label}>Todo</Label>
-              <Input
-                style={styles.textInput}
-                returnKeyType="send"
-                keyboardAppearance="dark"
-                onBlur={toggleInput}
-                getRef={(c: any) => (textInput.current = c._root)}
-              />
-            </Item>
-            <Button style={styles.addBtn} rounded>
-              <Text>Sup</Text>
-            </Button>
-          </Form>
-        )}
+        <Animated.View
+          style={[styles.form, { transform: [{ translateX: transX }] }]}
+        >
+          <Item style={styles.inputGroup}>
+            <Input
+              style={styles.textInput}
+              placeholder="Todo"
+              returnKeyType="send"
+              keyboardAppearance="dark"
+              onBlur={toggleInput}
+              ref={textInput}
+            />
+          </Item>
+          <Button style={styles.addBtn}>
+            <Text>Sup</Text>
+          </Button>
+        </Animated.View>
       </KeyboardAvoidingView>
       <Button rounded style={styles.fabBtn} onPress={toggleInput}>
         <Icon name="flame" />
@@ -85,26 +132,27 @@ const styles = StyleSheet.create({
   form: {
     position: 'relative',
     bottom: 70,
+    transform: [{ translateX: INPUT_HIDDEN_OFFSET }],
     marginHorizontal: 1,
     backgroundColor: '#444',
     flexDirection: 'row',
-    borderRadius: 28,
+    alignItems: 'flex-end',
+    borderRadius: 8,
   },
   inputGroup: {
     flexGrow: 1,
+    marginRight: 5,
     marginBottom: 3,
-    marginLeft: 30,
+    marginLeft: 5,
     height: 40,
   },
   textInput: {
     color: 'white',
   },
-  label: {
-    marginTop: -15,
-    color: 'grey',
-  },
   addBtn: {
-    height: '100%',
+    height: '90%',
+    borderRadius: 8,
+    margin: 2,
   },
   container: {
     position: 'absolute',
